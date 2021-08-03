a2ed1fbc3b7ae61d56897fee200be0a18d4fd997
Gourav raghuwanshi <53341811+gouravraghu@users.noreply.github.com>
2021-08-03 21:30:00 +0530

    Update Temperature.js

--- src/weather/Temperature.js
+++ src/weather/Temperature.js
@@ -8,7 +8,7 @@ const Temperature = () => {
 
     const getWeatherInfo = async () => {
         try {
-            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=f17a7e3f43c3ef8c67a5ba2ff65c2af0`;
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid={use your api key here}`;
 
             let res = await fetch(url);
             let data = await res.json();
