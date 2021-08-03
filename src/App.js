0c9154c46397ec0a285cfa7e36f5688fcb1f1211
gouravraghu <gouravraghuwanshi987@gmail.com>
2021-08-03 20:34:42 +0530

    weather app using react and open weather api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import Temperature from './weather/Temperature';
+//import WeatherCard from './weather/WeatherCard';
+
+function App() {
+  return (
+    <div className="App">
+      <Temperature/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/weather/Temperature.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import Card from './Card';
+import "./style.css";
+
+const Temperature = () => {
+    const [searchValue, setSearchValue] = useState("Indore");
+    const [tempInfo, setTempInfo] = useState({});
+
+    const getWeatherInfo = async () => {
+        try {
+            let url = `https://api.openweathermap.org/data/2.5/weather?q=${searchValue}&units=metric&appid=f17a7e3f43c3ef8c67a5ba2ff65c2af0`;
+
+            let res = await fetch(url);
+            let data = await res.json();
+
+            const { temp, humidity, pressure } = data.main;
+            const { main: weathermood } = data.weather[0];
+            const { name } = data;
+            const { speed } = data.wind;
+            const { country, sunset } = data.sys;
+
+            const myNewWeatherInfo = {
+                temp,
+                humidity,
+                pressure,
+                weathermood,
+                name,
+                speed,
+                country,
+                sunset,
+            };
+
+            setTempInfo(myNewWeatherInfo);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getWeatherInfo();
+    }, []);
+
+    return (
+        <>
+            <div className="wrap">
+                <div className="search">
+                    <input
+                        type="search"
+                        placeholder="search..."
+                        autoFocus
+                        id="search"
+                        className="searchTerm"
+                        value={searchValue}
+                        onChange={(e) => setSearchValue(e.target.value)}
+                    />
+
+                    <button
+                        className="searchButton"
+                        type="button"
+                        onClick={getWeatherInfo}>
+                        Search
+                    </button>
+                </div>
+            </div>
+
+            {/* our temp card  */}
+            <Card {...tempInfo} />
+        </>
+    );
+};
+
+
+export default Temperature;
